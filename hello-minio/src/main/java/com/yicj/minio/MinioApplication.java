a5b6b2ac60bf03502bded4b2fd635c2162efc444
易成杰 <626659321@qq.com>
2023-08-12 22:38:19 +0800

    mybatis 自定义AbstractMethod

---
+++ hello-minio/src/main/java/com/yicj/minio/MinioApplication.java
@@ -0,0 +1,16 @@
+package com.yicj.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: yicj
+ * @date: 2023/8/12 22:37
+ */
+@SpringBootApplication
+public class MinioApplication {
+    // https://blog.csdn.net/qq_18843587/article/details/130427757
+    public static void main(String[] args) {
+        SpringApplication.run(MinioApplication.class);
+    }
+}
