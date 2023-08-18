c23c47631901f1ce3b4627eea2b6988f10a1c497
yichengjie <626659321@qq.com>
2023-08-18 13:08:59 +0800

    test gateway

---
+++ hello-gateway/src/main/java/org/yicj/gateway/controller/HelloController.java
@@ -0,0 +1,25 @@
+package org.yicj.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yicj
+ * @date 2023年08月18日 13:05
+ */
+@RefreshScope
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Value("${nacos.username}")
+    private String username ;
+
+    @RequestMapping("/index")
+    public String index(){
+
+        return "username : " + username ;
+    }
+}
