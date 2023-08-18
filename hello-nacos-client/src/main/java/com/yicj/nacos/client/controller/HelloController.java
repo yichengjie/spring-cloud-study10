d80bcae5094bf6d3a567b13a43732d3144d922f4
yichengjie <626659321@qq.com>
2023-08-18 13:33:33 +0800

    test gateway

---
+++ hello-nacos-client/src/main/java/com/yicj/nacos/client/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.yicj.nacos.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yicj
+ * @date 2023年08月18日 13:26
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/index")
+    public String index(){
+
+        return "hello nacos client index !" ;
+    }
+
+}
