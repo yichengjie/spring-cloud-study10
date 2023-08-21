036bcdc78df028da092e8683c2a70e48fea59f06
yichengjie <626659321@qq.com>
2023-08-21 22:00:10 +0800

    gateway body param cache

--- hello-gateway/src/main/java/com/yicj/gateway/filter/CustomGatewayFilterFactory.java
+++ hello-gateway/src/main/java/com/yicj/gateway/filter/CustomGatewayFilterFactory.java
@@ -6,8 +6,16 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author yicj
@@ -21,9 +29,31 @@ public class CustomGatewayFilterFactory extends AbstractGatewayFilterFactory<Cus
         super(Config.class);
     }
 
+    //https://huaweicloud.csdn.net/638752b9dacf622b8df8ad23.html
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
+
+            ServerHttpRequest request = exchange.getRequest();
+            Flux<DataBuffer> body = request.getBody();
+//            body.subscribe(buffer -> {
+//                byte [] bytes = new byte[buffer.readableByteCount()] ;
+//                buffer.read(bytes) ;
+//                DataBufferUtils.release(buffer) ;
+//                String bodyString = new String(bytes);
+//                log.info("Body String : {}", bodyString);
+//            }) ;
+
+//            AtomicReference<String> bodyRef = new AtomicReference<>();
+//            body.subscribe(buffer -> {
+//                CharBuffer charBuffer = StandardCharsets.UTF_8.decode(buffer.asByteBuffer());
+//                DataBufferUtils.release(buffer);
+//                bodyRef.set(charBuffer.toString());
+//            });
+//            //获取request body
+//            String bodyStr = bodyRef.get();
+//            log.info(bodyStr);
+
             if (config.isPreLogger()) {
                 log.info("CustomGatewayFilterFactory pre message is {}", config.getMessage());
             }
