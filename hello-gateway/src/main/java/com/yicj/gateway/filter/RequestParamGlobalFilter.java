52027bb849d98dbddbbe755ddb4453cd766bc4eb
yichengjie <626659321@qq.com>
2023-08-21 22:27:27 +0800

    gateway body param cache

--- hello-gateway/src/main/java/com/yicj/gateway/filter/RequestParamGlobalFilter.java
+++ hello-gateway/src/main/java/com/yicj/gateway/filter/RequestParamGlobalFilter.java
@@ -7,6 +7,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.HttpMessageReader;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -17,6 +18,8 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.Charset;
 import java.util.List;
 
 
@@ -31,20 +34,36 @@ public class RequestParamGlobalFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        /**
-         * save request path and serviceId into gateway context
-         */
-        ServerHttpRequest request = exchange.getRequest();
-        HttpHeaders headers = request.getHeaders();
-        // 处理参数
-        MediaType contentType = headers.getContentType();
-        long contentLength = headers.getContentLength();
-        if (contentLength > 0) {
-            if (MediaType.APPLICATION_JSON.equals(contentType) || MediaType.APPLICATION_JSON_UTF8.equals(contentType)) {
-                return readBody(exchange, chain);
-            }
-        }
-        return chain.filter(exchange);
+//        /**
+//         * save request path and serviceId into gateway context
+//         */
+//        ServerHttpRequest request = exchange.getRequest();
+//        HttpHeaders headers = request.getHeaders();
+//        // 处理参数
+//        MediaType contentType = headers.getContentType();
+//        long contentLength = headers.getContentLength();
+//        if (contentLength > 0) {
+//            if (MediaType.APPLICATION_JSON.equals(contentType) || MediaType.APPLICATION_JSON_UTF8.equals(contentType)) {
+//                return readBody(exchange, chain);
+//            }
+//        }
+//        return chain.filter(exchange);
+//        ServerHttpRequest request = exchange.getRequest();
+//        HttpHeaders headers = request.getHeaders();
+//        HttpMethod method = request.getMethod();
+//        return DataBufferUtils.join(exchange.getRequest().getBody())
+//                .flatMap(dataBuffer -> {
+//                    byte[] bytes = new byte[dataBuffer.readableByteCount()];
+//                    dataBuffer.read(bytes);
+//                    String body = new String(bytes, Charset.forName("UTF-8"));
+//                    // 此处可以对请求体进行处理
+//                    exchange.getAttributes().put("cachedRequestBodyObject", body);
+//                    return chain.filter(exchange.mutate().request(request.mutate()
+//                            .header("Content-Type", "application/json")
+//                            .body(Mono.just(DataBufferUtils.createByteBuffer(body.getBytes())))).build());
+//                });
+
+        return null ;
     }
 
 
