a258b17aaceb4d7f38aa45d0deb323126b0b7217
yichengjie <626659321@qq.com>
2023-08-29 21:42:20 +0800

    gateway body param cache

--- hello-nacos-client/src/main/java/com/yicj/nacos/client/controller/HelloController.java
+++ hello-nacos-client/src/main/java/com/yicj/nacos/client/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.yicj.nacos.client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * @author yicj
@@ -19,8 +24,18 @@ public class HelloController {
     @Autowired
     private WebClient.Builder builder ;
 
+    @Autowired
+    private DiscoveryClient discoveryClient ;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient ;
+
+
     @GetMapping("/index")
     public String index(){
+        String serviceId = "nacos-client-app" ;
+        //ServiceInstance choose = loadBalancerClient.choose(serviceId);
+        List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
 
         return "hello nacos client index !" ;
     }
