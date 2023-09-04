0e091895097001cf1aefc7d4b7dc9a340a014c9a
yichengjie <626659321@qq.com>
2023-09-04 22:49:45 +0800

    save

--- hello-webflux/src/test/java/com/yicj/webflux/Flux2Test.java
+++ hello-webflux/src/test/java/com/yicj/webflux/Flux2Test.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Flux2Test {
@@ -53,5 +56,33 @@ public class Flux2Test {
         // 订阅时触发request -> sink.next顺序产生数据
         // 生产一个数据消费一个
         flux.subscribe(value -> log.info("value : {}", value)) ;
+    }
+
+    @Test
+    public void flatMap() throws InterruptedException {
+        Flux.just(DayOfWeek.SUNDAY, DayOfWeek.MONDAY)
+                .flatMap(this::loadRecordFor, 2)
+                .subscribe(value -> log.info("value : {}", value)) ;
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    @Test
+    public void concatMap() throws InterruptedException {
+        Flux.just(DayOfWeek.SUNDAY, DayOfWeek.MONDAY)
+                .concatMap(this::loadRecordFor)
+                .subscribe(value -> log.info("value : {}", value)) ;
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    private Flux<String> loadRecordFor(DayOfWeek daw){
+        switch (daw){
+            case SUNDAY:  return Flux.interval(Duration.ofMillis(90))
+                    .take(5)
+                    .map(i -> "Sun" + i) ;
+            case MONDAY:  return Flux.interval(Duration.ofMillis(65))
+                    .take(5)
+                    .map(i -> "Mon" + i) ;
+            default:  return Flux.empty() ;
+        }
     }
 }
