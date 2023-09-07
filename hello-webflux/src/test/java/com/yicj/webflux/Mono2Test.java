7539b0447cad48fbc0621b417bf83c654d1271ed
yichengjie <626659321@qq.com>
2023-09-07 13:41:57 +0800

    阻塞api改造

--- hello-webflux/src/test/java/com/yicj/webflux/Mono2Test.java
+++ hello-webflux/src/test/java/com/yicj/webflux/Mono2Test.java
@@ -2,13 +2,23 @@ package com.yicj.webflux;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 public class Mono2Test {
 
     @Test
-    public void transform(){
-
+    public void subscribeOn() throws InterruptedException {
+        Mono.just(1)
+                .map(item -> {
+                    log.info("map item : {}", item);
+                    return "hello["+item+"]" ;
+                })
+                .then(Mono.defer(() -> Mono.just(2).subscribeOn(Schedulers.boundedElastic())))
+                //.subscribeOn(Schedulers.parallel())
+                .subscribe(value -> log.info("result value : {}", value)) ;
+        Thread.sleep(1000);
     }
 
 }
