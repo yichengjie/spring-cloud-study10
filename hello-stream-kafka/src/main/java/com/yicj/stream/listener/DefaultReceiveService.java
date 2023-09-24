302a15d56965eb4adf790272f15c91ffd863bf22
易成杰 <626659321@qq.com>
2023-09-24 22:30:09 +0800

    kafka trail

--- hello-stream-kafka/src/main/java/com/yicj/stream/listener/DefaultReceiveService.java
+++ hello-stream-kafka/src/main/java/com/yicj/stream/listener/DefaultReceiveService.java
@@ -6,15 +6,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 
 @Slf4j
 @EnableBinding(Sink.class)
 public class DefaultReceiveService {
  
+//    @StreamListener(Sink.INPUT)
+//    public void receive(@Payload Object payload){
+//        HelloMessage message = JSON.parseObject(payload.toString(), HelloMessage.class);
+//        log.info("========> consumer receive message : {}", message);
+//    }
+
+
     @StreamListener(Sink.INPUT)
-    public void receive(@Payload Object payload){
-        HelloMessage message = JSON.parseObject(payload.toString(), HelloMessage.class);
-        log.info("========> consumer receive message : {}", message);
+    public void process(Message<?> message) {
+        Acknowledgment acknowledgment = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
+        if (acknowledgment != null) {
+            System.out.println("Acknowledgment provided");
+            acknowledgment.acknowledge();
+        }
     }
 }
