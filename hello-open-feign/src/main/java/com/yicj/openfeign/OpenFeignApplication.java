0d36d259ec59c51367c1b5d558c191d63e9062c6
yichengjie <626659321@qq.com>
2023-09-27 09:56:41 +0800

    open feign invoker remote method test

--- hello-open-feign/src/main/java/com/yicj/openfeign/OpenFeignApplication.java
+++ hello-open-feign/src/main/java/com/yicj/openfeign/OpenFeignApplication.java
@@ -1,13 +1,21 @@
 package com.yicj.openfeign;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 /**
  * @author yicj
  * @date 2023年08月30日 17:25
  */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
 public class OpenFeignApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        SpringApplication.run(OpenFeignApplication.class, args) ;
     }
 }
---
+++ hello-open-feign/src/main/java/com/yicj/openfeign/client/HelloClient.java
@@ -0,0 +1,16 @@
+package com.yicj.openfeign.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "nacos-client-app"
+    , contextId = "echoClient"
+//    , url = "127.0.0.1:8081"
+)
+public interface HelloClient {
+
+    @GetMapping("/hello/index")
+    String index() ;
+
+}
