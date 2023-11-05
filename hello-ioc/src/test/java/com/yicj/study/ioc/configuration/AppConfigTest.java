ba495f1f4a64e9d437f75865e075165edaa527e0
易成杰 <626659321@qq.com>
2023-11-05 17:32:27 +0800

    补充doc

--- hello-ioc/src/test/java/com/yicj/study/ioc/configuration/AppConfigTest.java
+++ hello-ioc/src/test/java/com/yicj/study/ioc/configuration/AppConfigTest.java
@@ -4,7 +4,13 @@ import com.yicj.study.ioc.configuration.model.Pet;
 import com.yicj.study.ioc.configuration.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.util.LambdaSafe;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.server.WebServerFactoryCustomizerBeanPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author yicj
@@ -25,5 +31,34 @@ public class AppConfigTest {
         //true 可以证明向容器中注入的组件是单实例的
         log.info("组件：" + (tom01 == tom02));
         log.info("用户的宠物：" + (tom01.getPet() == pet));
+    }
+
+    @Test
+    public void customize(){
+        HelloFactory helloFactory = new HelloFactory() ;
+        //
+        LambdaSafe.callbacks(Customizer.class, getCustomizers(), helloFactory)
+                .withLogger(WebServerFactoryCustomizerBeanPostProcessor.class)
+                .invoke((customizer) -> customizer.customize(helloFactory));
+    }
+
+
+    private List<Customizer> getCustomizers(){
+        return List.of(new Customizer(){
+
+            @Override
+            public void customize(HelloFactory factory) {
+                log.info("customize execute... ");
+            }
+        }) ;
+    }
+
+
+    interface Customizer{
+        void customize(HelloFactory factory) ;
+    }
+
+    class HelloFactory{
+
     }
 }
